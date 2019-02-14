8dae9f5b1cf8b7d415bd1fa40ac19815803a6f7c
lhuddlesto <lhuddlesto@gannett.com>
2019-02-14 09:31:51 -0500

    First commit

---
+++ app.py
@@ -0,0 +1,17 @@
+import random
+playerChoice = input("rock, paper, or scissors? \n")
+choices = ['rock', 'paper', 'scissors']
+compChoice = random.choice(choices)
+
+if compChoice == playerChoice:
+    print(f"It's a tie!  You both chose {compChoice}.  Try again?")
+
+elif (
+    (compChoice == 'rock' and playerChoice == 'scissors') or 
+    (compChoice == 'paper' and playerChoice == 'rock') or
+    (compChoice == 'scissors' and playerChoice == 'paper')
+    ):
+    print(f"You lost! {compChoice} beats {playerChoice}. Try again?")
+
+else:
+    print(f"You won! {playerChoice} beats {compChoice}.")
